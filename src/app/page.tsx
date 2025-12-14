503be371933ec8c257c0ab1ab09ea3210b002155
Rachana Biswas <ms.rachanabiswas@gmail.com>
2025-12-14 18:05:13 +0530

    project setup

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import ToastButton from "@/components/Buttons/ToastButton";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Next.js Starter Fullstack",
+	description: "Production grade Fullstack Next.js starter template",
+};
+
+const page = () => {
+	return (
+		<section className="grid h-[90dvh] place-items-center">
+			<div className="space-y-4 text-center">
+				<h1 className="text-5xl font-semibold">Next.js Starter Fullstack</h1>
+				<h2 className="text-3xl">
+					Production grade Fullstack Next.js starter template
+				</h2>
+
+				<ToastButton />
+			</div>
+		</section>
+	);
+};
+
+export default page;
